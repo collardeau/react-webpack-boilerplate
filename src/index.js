3512fc28fe7de8220569a893e12fce1251b81e8c
Thomas Collardeau <thomas.collardeau@clevertech.biz>
2015-11-16 23:40:18 +0000

    pass in props to react component (appState)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
+const App = ({appState}) => {
   return (
     <div>
-      <Header />
+      <Header {...appState}/>
+      <div> { appState.dynamicText} </div>
     </div> 
   )
 }
 
-const Header = () => {
+const Header = ({header}) => {
+  const styles = {
+    header: {
+      "fontSize": header.big ? "24px": "12px",
+      "border": "1px solid"
+    }
+  }
+
   return (
-    <div>Header</div> 
+    <div style={styles.header}>Header</div>
   );
 }
 
+const state = {
+  dynamicText: "some ui state",
+  header: {
+    big: true 
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <App appState={state}/>,
   document.getElementById('app')
 );
 
